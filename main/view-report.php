b88c992692edb85800f97c55cb24791c3c389fec
QuanTBacon <bryanlai820@gmail.com>
2019-06-08 11:35:42 +0800

    Updated view report page

--- main/view-report.php
+++ main/view-report.php
@@ -2,6 +2,26 @@
 require_once('banner.php');
 ?>
 
+<?php
+require_once('../dbcon.php');
+
+$rid = $_GET['ReportID'];
+
+$sql = "SELECT * FROM damage WHERE ReportID = '$rid'";
+$result = mysqli_query($con, $sql);
+
+if($result) {
+    while($row = $result->fetch_assoc()) {
+        $title = $row['ReportTitle'];
+        $desc = $row['ReportDesc'];
+        $quantity = $row['DamageQuantity'];
+
+    }
+} else {
+
+}
+?>
+
 <html>
     <head>
         <title>View Report | Inventory System</title>
@@ -16,14 +36,23 @@ require_once('banner.php');
             <table class="report-table">
                 <tr>
                     <th>Report Title</th>
+                    <td><?php echo $title; ?></td>
+                </tr>
+                <tr>
+                    <th>Reported By</th>
                     <td>lalala</td>
                 </tr>
                 <tr>
                     <th>Report Description</th>
+                    <td><?php echo $desc; ?></td>
+                </tr>
+                <tr>
+                    <th>Affected Item</th>
                     <td>lalala</td>
                 </tr>
                 <tr>
-
+                    <th>Affected Amount</th>
+                    <td><?php echo $quantity; ?></td>
                 </tr>
             </table>
         </div>
