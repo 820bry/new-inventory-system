c05e949eb4a2288c88ef760c632a870df4bf0d25
QuanTBacon <bryanlai820@gmail.com>
2019-06-18 13:53:15 +0800

    Updated inventory.php

--- main/inventory.php
+++ main/inventory.php
@@ -25,10 +25,11 @@ require_once('banner.php');
                 </form>
             </div>
         </div>
-        
+    
         <div class="page-content">
             <h2>Inventory</h2>
-            <button id ="btn-search" class="btn-search">Search Inventory</button>
+            <input type="text" id="search-bar" name="search_bar" class="search" placeholder="Enter Search Term">
+            <button id ="btn-search" class="btn-search">&#128270</button>
             <button id="btn-import" class="btn-import">Import Table Data</button>
             <button id="btn-add" class="btn-add">Add Item</button>
             <table class="inventory-listing">
