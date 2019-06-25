4f6a6b08c1a0232993f5f0168a068b2f050adb25
QuanTBacon <bryanlai820@gmail.com>
2019-06-25 22:33:30 +0800

    Finished main.php

--- main/main.php
+++ main/main.php
@@ -13,10 +13,68 @@ include('../banner.php');
     </head>
     <body>
         <div class="page-content">
-            <h2>Tentang Kelab i-CreatorZ</h2>
-            <p>
-            Kelab i-CreatorZ ditubuhkan pada tahun 2018 dengan tujuan untuk melahirkan pelajar generasi<br>
-            akan datang dengan minda kreatif
+            <br>
+            <div class="interests">
+                <div class="section">
+                    <span class="number">01</span>
+                    <span class="text">Teknologi</span>
+                </div>
+                <div class="section">
+                    <span class="number">02</span>
+                    <span class="text">Inovasi</span>
+                </div>
+                <div class="section">
+                    <span class="number">03</span>
+                    <span class="text">Penyelidikan & Pembangunan Sains (R&D)</span>
+                </div>
+                <div class="section">
+                    <span class="number">04</span>
+                    <span class="text">Kreativiti</span>
+                </div>
+            </div>
+            <br>
+            <hr>
+            <div class="introduction">
+                <h3>MISI KITA</h3>
+                <p>
+                Kita menyediakan pelajar-pelajar dengan platform untuk menjalankan
+                penyelidikan dan menjalankan projek berdasarkan minat individu
+                untuk faedah sekolah dan diri sendiri.
+                </p>
+                <h3>VISI KITA</h3>
+                <p>
+                Untuk memupuk minat sains dan inovasi dalam kalangan pelajar.
+                </p>
+            </div>
+            <br>
+            <hr>
+            <br>
+            <div class="timeline">
+                <h3>PERISTIWA-PERISTIWA YANG MEMBAWA KEPADA PENUBUHAN I-CREATORZ</h3>
+                <span class="year">2014</span>
+                <p class="desc">
+                Mr. Soo, Guru Besar SMJK Chung Ling ingin mengembangkan 
+                bidang sains di sekolah dan menganjurkan karnival sains pertama.
+                </p>
+                <span class="year">2015</span>
+                <p class="desc">
+                Sebuah kumpulan Facebook yang bernama "CLHS Innovation and Research 
+                Group" ditubuhkan.
+                </p>
+                <span class="year">2016</span>
+                <p class="desc">
+                Mendapati keputusan yang cemerlang dalam karnival sains JPN.
+                </p>
+                <span class="year">2017</span>
+                <p class="desc">
+                Permulaan rancangan untuk menubuhkan sebuah kelab yang memberi tumpunan
+                kepada sains dan inovasi.
+                </p>
+                <span class="year">2018</span>
+                <p class="desc">
+                Pelancaran rasmi dan permulaan kelab ke-83 dalam SMJK Chung Ling, i-CreatorZ.
+                </p>
+            </div>
             </p>
         </div>
     </body>
