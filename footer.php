9debd1c0006bb37360af2969dbfc3fb5a841d238
QuanTBacon <bryanlai820@gmail.com>
2019-07-25 12:27:27 +0800

    Added content resizing

--- footer.php
+++ footer.php
@@ -8,8 +8,23 @@
     <br>
     <hr>
         <div class="footer">
+            <div class="resize">
+                <button onclick="resizeText('-1');">-</button>
+                <span>Resize Content</span>
+                <button onclick="resizeText('1');">+</button>
+            </div>
             <img src="../styling/Images/logoblack.png" alt="logo-bw" width="30px" height="30px">
             <p><b>Hak Cipta | Kelab i-CreatorZ, SMJK Chung Ling</b></p>
         </div>
+
+        <script type="text/javascript">
+            function resizeText(factor) {
+                if(document.body.style.fontSize == "") {
+
+                    document.body.style.fontSize = "1.0em";
+                }
+                document.body.style.fontSize = parseFloat(document.body.style.fontSize) + (factor * 0.2) + "em";
+            }
+        </script>
     </body>
 </html>
